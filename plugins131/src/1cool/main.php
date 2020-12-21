30db13765befe7ca18b2c14a982919964643e00b
Lucaert <67279858+Lucaert@users.noreply.github.com>
2020-12-21 14:05:56 +0100

    Add files via upload

---
+++ plugins131/src/1cool/main.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Ichbincool;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool{
+        if($cmd->getName() --- "ichsagdubistcool"){
+            if($sender->hasPermission("ichsagdubistcool.cmd"){
+                $sender->sendMessage("IchmagBananen"):
+                
+            }
+        }
+    }
+    return true;
+}
+
+    
+
+    
+    
+    
+       
+
